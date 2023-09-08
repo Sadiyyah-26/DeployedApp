579f3e14aee3fad21a307d32f5972ae12c8d8316
Bastika <basti03prem@gmail.com>
2023-09-08 18:32:48 +0200

    Donations code

---
+++ ShoppingCartMVC/Models/tblDonations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartMVC.Models
+{
+    public class tblDonations
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DonationId { get; set; }
+
+        public string DonCharityOrg { get; set; }
+
+        public string DonEmail { get; set; }
+
+        public int DonationAmount { get; set; }
+    }
+}
