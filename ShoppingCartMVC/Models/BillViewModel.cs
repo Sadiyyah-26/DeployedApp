e86b4edec5b9f5ce1fde81ed214bf5eba4b8b7c0
Viyosha <viyoshag@gmail.com>
2023-09-11 04:13:44 -0700

    Part1POS
    Part1

---
+++ ShoppingCartMVC/Models/BillViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartMVC.Models
+{
+    public class BillViewModel
+    {
+        public int BillId { get; set; }
+
+        public string OrderNumber { get; set; }
+
+        public string WaiterName { get; set; }
+
+        public string TableNumber { get; set; }
+        public List<SelectedProductModel> Products { get; internal set; }
+        public int TotalAmount { get; internal set; }
+        public string ProductName { get; internal set; }
+        public int? Qty { get; internal set; }
+        public int? Total { get; internal set; }
+
+        public string PaymentMethod { get; internal set; }
+    }
+}
---
+++ ShoppingCartMVC/Models/InStoreSuccessCashViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartMVC.Models
+{
+    public class InStoreSuccessCashViewModel
+    {
+        public string OrderNumber { get; set; }
+        public DateTime OrderDateTime { get; set; }
+        public List<SelectedProductModel> Products { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
